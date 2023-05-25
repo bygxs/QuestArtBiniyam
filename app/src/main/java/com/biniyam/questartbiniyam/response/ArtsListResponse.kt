e4c7d91258491de983320301b0273478a81f4047
bygxs <114972065+bygxs@users.noreply.github.com>
2023-05-25 12:44:41 +0200

    25 MAY 2025 12:25 response JSON body converted to 'response' class; using JSON to Kotlin class tool.  data class Config.kt contains image url and website url

--- app/src/main/java/com/biniyam/questartbiniyam/response/ArtsListResponse.kt
+++ app/src/main/java/com/biniyam/questartbiniyam/response/ArtsListResponse.kt
@@ -1,5 +1,7 @@
 package com.biniyam.questartbiniyam.response
 
+
+// this file is derived from api JSON response and converted to kotlin classes
 data class ArtsListResponse(
     val config: Config,
     val `data`: List<Data>,
--- app/src/main/java/com/biniyam/questartbiniyam/response/Config.kt
+++ app/src/main/java/com/biniyam/questartbiniyam/response/Config.kt
@@ -1,5 +1,9 @@
 package com.biniyam.questartbiniyam.response
 
+
+// this file is derived from api JSON response and converted to kotlin classes
+
+// this particular class contains the image url and website which is the most essential for the project.
 data class Config(
     val iiif_url: String,
     val website_url: String
