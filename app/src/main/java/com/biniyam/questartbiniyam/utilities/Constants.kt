f3c871b4d85a06c6778818170e78641098033ca1
bygxs <114972065+bygxs@users.noreply.github.com>
2023-05-25 12:16:41 +0200

    25 MAY 2025 12:14 Project setup-  Gradel configured  packages created  Constants.kt object created saving base url and image url

---
+++ app/src/main/java/com/biniyam/questartbiniyam/utilities/Constants.kt
@@ -0,0 +1,9 @@
+package com.biniyam.questartbiniyam.utilities
+
+object Constants {
+
+    const val API_KEY = "Not required when required"
+    const val BASE_URL = "https://api.artic.edu/api/v1/artworks"
+    const val IIIF_IMAGE_API_= "https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg"
+
+}
