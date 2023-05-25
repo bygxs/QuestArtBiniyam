6e83cce4e249f67399081f725208e8561dd02fce
bygxs <114972065+bygxs@users.noreply.github.com>
2023-05-25 12:53:13 +0200

    25 MAY 2025 12:53
    added comments

--- app/src/main/java/com/biniyam/questartbiniyam/response/Pagination.kt
+++ app/src/main/java/com/biniyam/questartbiniyam/response/Pagination.kt
@@ -1,5 +1,8 @@
 package com.biniyam.questartbiniyam.response
 
+// this file is derived from api JSON response and converted to kotlin classes
+
+// this class enables to page through art works using next and previous page buttons, if needed
 data class Pagination(
     val current_page: Int,
     val limit: Int,
